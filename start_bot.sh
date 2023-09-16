5bc7bd0011ed98b5054773dd711fd3d43afe740c
Marcelo Lima Gomes <marcelolimagomes@gmail.com>
2023-09-16 10:07:52 -0400

    Start Bot script

--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+# This is a basic shell script template
+
+# Author: Marcelo Lima Gomes
+# Date: 11/09/2023
+
+DATE_FORMATED=$(date +%Y%m%d_%H%M%S)
+
+echo "Starting bot at $(date +%Y-%m-%d_%H:%M:%S)..."
+
+source .env/bin/activate
+
 python . -run-bot -calc-rsi -start-train-date=2023-06-01 -all-cols -regression-times=0 -stop-loss=2.5 -estimator=knn -symbol=ADAUSDT >> bot_ADAUSDT.log
 python . -run-bot -calc-rsi -start-train-date=2023-06-01 -all-cols -regression-times=0 -stop-loss=2.5 -estimator=knn -symbol=AVAXUSDT >> bot_AVAXUSDT.log
 python . -run-bot -calc-rsi -start-train-date=2023-06-01 -all-cols -regression-times=0 -stop-loss=1.5 -estimator=knn -symbol=BCHUSDT >> bot_BCHUSDT.log
@@ -10,3 +22,4 @@ python . -run-bot -calc-rsi -start-train-date=2023-06-01 -all-cols -regression-t
 python . -run-bot -calc-rsi -start-train-date=2023-06-01 -all-cols -regression-times=0 -stop-loss=1.5 -estimator=knn -symbol=TRXUSDT >> bot_TRXUSDT.log
 python . -run-bot -calc-rsi -start-train-date=2023-06-01 -all-cols -regression-times=0 -stop-loss=3.0 -estimator=knn -symbol=XRPUSDT >> bot_XRPUSDT.log
 
+exit 0
